71ae8aaf7a2116d9bd62b6a5c9fc97656c64e937
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-12-12 14:16:37 +0530

    Created: EmployeePayRollApp in Spring created and configured

---
+++ src/main/java/com/bridgelabz/EmployeepayRollAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeepayRollAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeepayRollAppSpringApplication.class, args);
+	}
+
+}
