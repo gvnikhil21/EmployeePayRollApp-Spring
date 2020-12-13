83b8e0696251eb50a21f4680d0f5b93feb314e3d
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-12-14 00:10:45 +0530

    Added: Environment variables for the db username and password was set in application.properties file and tested

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppSpringApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppSpringApplication.java
@@ -13,6 +13,7 @@ public class EmployeePayRollAppSpringApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(EmployeePayRollAppSpringApplication.class, args);
 		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB user is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
